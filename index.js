4f3ff5473da3516a792fedbe1dbb21eb9ead1f86
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-02 20:08:09 +0900

    V01_SETUP

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var express = require('express');
 var request = require('request');
 var app = express();
+var http, options, proxy, url;
+
+http = require("http");
+
+url = require("url");
+
+proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
+target  = url.parse("http://ip.quotaguard.com/");
+
+options = {
+  hostname: proxy.hostname,
+  port: proxy.port || 80,
+  path: target.href,
+  headers: {
+    "Proxy-Authorization": "Basic " + (new Buffer(proxy.auth).toString("base64")),
+    "Host" : target.hostname
+  }
+};
+
+http.get(options, function(res) {
+  res.pipe(process.stdout);
+  return console.log("status code", res.statusCode);
+});
+
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
