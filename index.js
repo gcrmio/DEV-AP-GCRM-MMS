a544029b2f126b2400f167b81f0a8d2d0de4892c
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-02 21:51:36 +0900

    V01_SETUP

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+'use strict';
 var express = require('express');
 var request = require('request');
 var app = express();
+require('dotenv').load({silent: true});
+
+var url = require('url');
+var HttpsProxyAgent = rerquire('https-proxy-agent');
+var request = require('request');
+
+var testEndpoint = 'https://ap-gcrm-mms.herokuapp.com/';
+var proxy = process.env.QUOTAGUARDSHIELD_URL;
+var agent = new HttpsProxyAgent(proxy);
+var options = {
+    url: url.parse(testEndpoint),
+    agent
+};
+
+function callback(error, response, body){
+    if(!error && response.statusCode == 200){
+        console.log('body: ', body);
+    } else{
+        console.log('error: ', error);
+    }
+}
+
+try {
+    console.log('CALLBACK =====================================');
+    request(options, callback); 
+} catch (error) {
+    console.log('there was an error');
+}
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
