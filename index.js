3a791e7a5bea020a1de02f51e1f44562ab7b14f2
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-04 10:58:24 +0900

    V01_SETUP

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-//var pg = require('pg');
+var pg = require('pg');
+/*
 var http, options, proxy, url;
-
 http = require("http");
-
 url = require("url");
-
 proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
-target  = url.parse("https://ap-gcrm-mms.herokuapp.com/");
 
+//proxy setup
+target  = url.parse("https://ap-gcrm-mms.herokuapp.com/");
 options = {
   hostname: proxy.hostname,
   port: proxy.port || 80,
@@ -25,7 +24,9 @@ http.get(options, function(res) {
   res.pipe(process.stdout);
   return console.log("status code", res.statusCode);
 });
+*/
 
+//Render Every8D page
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -35,21 +36,18 @@ function(){
     console.log("ap-gcrm-mms heroku app is running on ["+app.get("port")+"]");
 });
 
-app.get('/', (req, res) => {
-    res.send('AP-GCRM-MMS APP');
-})
-
-/*PG Setup
+//PG Setup
 const dbconfig = {
-    host: process.env.host,
-    user: process.env.user,
-    password: process.env.password,
-    database: process.env.database,
-    port: process.env.port,
+    host: process.env.DB_host,
+    user: process.env.DB_user,
+    password: process.env.DB_password,
+    database: process.env.DB_database,
+    port: process.env.DB_port,
     ssl: {
       rejectUnauthorized: false
     }
   }
+  
   console.log('PG Connect ==============================');
   const client = new pg.Client(dbconfig);
   client.connect(err =>{
@@ -59,9 +57,69 @@ const dbconfig = {
       console.log('Connect to db done!');
     }
   })
-  */
+
+app.get('/', (req, res) => {
+    res.send('AP-GCRM-MMS APP');
+})
 
 app.get('/Every8D', (req, res) => {
     console.log('Accessing Every8D');
     res.render('every8D.html');
 })
+
+app.get('/pgSelect', (req, res) => {
+    try {
+        console.log("PGSELECT=======================================");
+        dbSelect();
+        res.send('PG Select Complete!');
+      } catch (error) {
+        console.log('There was an error!');
+      }
+})
+
+//PG_SELECT FROM transmit
+function dbSelect(){
+    const sql = `SELECT cust_id, phone_no, msg_id, msg_subject_adj, msg_body_text_adj, msg_body_image_adj_file, msg_type, plan_date, send_date, success_yn FROM transmit`
+  
+    client.query(sql, (err, res) => {
+      if(err){
+        console.log(err.stack);
+      } else {
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+        console.log(res.rows);
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+        for(const row of res.rows){
+          var cust_id = row.cust_id;
+          var phone_no = row.phone_no;
+          var msg_id = row.msg_id;
+          var msg_subject_adj = row.msg_subject_adj;
+          var msg_body_text_adj = row.msg_body_text_adj;
+          var msg_body_image_adj_file = row.msg_body_image_adj_file;
+          var msg_type = row.msg_type;
+          var plan_date = row.plan_date;
+          var send_date = row.send_date;
+          var success_yn = row.success_yn;
+
+          console.log('length of cust_id = '+cust_id.length);
+          console.log('length of phone_no = '+phone_no.length);
+          console.log('length of msg_id = '+msg_id.length);
+          console.log('length of msg_subject_adj = '+msg_subject_adj.length);
+          console.log('length of msg_body_text_adj = '+msg_body_text_adj.length);
+          console.log('length of msg_body_image_adj_file = '+msg_body_image_adj_file.length);
+          console.log('length of msg_type = '+msg_type.length);
+          console.log('length of plan_date = '+plan_date.length);
+          console.log('length of send_date = '+send_date.length);
+          console.log('length of success_yn = '+success_yn.length);
+
+        //   try {
+            // send_message(mobile, sb, msg);
+            // console.log(sid+' has successfully sent');
+        //   } catch (error) {
+            // console.log('send message error');
+        //   }
+        }
+        // console.log('*************************************************************');
+        // console.log(res.rows);
+      }
+    })
+  }
