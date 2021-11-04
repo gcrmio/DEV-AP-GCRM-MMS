b95e0f648ce037f5080d60027d57c3831d4892ad
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-04 16:45:24 +0900

    V02_TEST

--- index.js
+++ index.js
@@ -127,6 +127,6 @@ function sendMsg(subject, msg, dest, time){
     request.get({
         url: geturl
     }, function(error, response, body){
-        response.json(JSON.parse(body));
+        console.log(response);
     })
 }
