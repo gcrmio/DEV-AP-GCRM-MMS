6969be2f3fa41ba353e579ccc7f343fd1eafbf7e
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-08 16:35:27 +0900

    V04_APPS

---
+++ receiveResult.js
@@ -0,0 +1,22 @@
+var request = require('request');
+
+function sendStat(BID){
+    const url = 'https://oms.every8d.com/API21/HTTP/getDeliveryStatus.ashx';
+    const uid = process.env.Euid;
+    const password = process.env.Epassword;
+    var bidValue = BID;
+    console.log('bidValue= '+bidValue);
+    var geturl = url+'?UID='+uid+'&PWD='+password+'&BID='+bidValue+'&PNO=';
+    console.log(geturl);
+    console.log('======================');
+    request.get({
+        url: geturl
+    }, function(error, response, html){
+        if(error){
+            console.log(error);
+        }
+        console.log('Received Server Data!');
+        console.log(html);
+    })
+}
+
