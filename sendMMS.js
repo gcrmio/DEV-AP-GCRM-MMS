02d431fae600a831e348c266e8dfba9cbd61a73a
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-09 09:22:18 +0900

    V02_TEST

--- sendMMS.js
+++ sendMMS.js
@@ -27,7 +27,7 @@ const dbconfig = {
 
 module.exports.dbSelect = function(){
     
-    const sql = `SELECT cust_id, phone_no, msg_id, msg_subject_adj, msg_body_text_adj, msg_body_image_adj_file, msg_type, plan_date, send_date, success_yn FROM transmit`
+    const sql = `SELECT cust_id, phone_no, msg_id, msg_subject_adj, msg_body_text_adj, msg_body_image_adj_file, msg_type, plan_date, send_date, success_yn FROM transmit WHERE cust_id = 'KR00000004'`
   
     client.query(sql, (err, res) => {
       if(err){
