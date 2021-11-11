bb0cd7dfa519f14d7f5af2cb1d3632a58c147c7c
wjchoi <wonjeung.choi@amorepacific.com>
2021-11-11 13:01:53 +0900

    V02

--- sendMMS.js
+++ sendMMS.js
@@ -34,13 +34,6 @@ AWS.config.update({
   region: 'us-east-1',
 });
 
-s3.listBuckets(function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  } else {
-    console.log("Success", data.Buckets);
-  }
-});
 
 /*
 module.exports.dbSelect = function(){
